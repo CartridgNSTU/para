b028f66d5296860b633a7bc3dd40d10260af0353
CartridgNSTU <97868606+CartridgNSTU@users.noreply.github.com>
2022-03-28 12:31:24 +0700

    Add files via upload

--- profile.php
+++ profile.php
@@ -3,16 +3,14 @@
 	if ($_GET['act'] === 'logout') {
 		unset($_SESSION['user_login']);
 	}
-	if (!empty($_POST['name'])&& !empty($_POST['pas'])){
-		$name = $_POST['name'];
-		$password = $_POST['pas'];
-		$_SESSION['user_name'] = $name;
-		$_SESSION['user_pas'] = $password;
-		// авторизация успешна...
-		if ($name === $_POST['name'] && $password === $_POST['pas']) {
-			$_SESSION['user_login'] = $name;
-			header('Location: index.php');
-		}
+	if (!empty($_POST['pername'])&& !empty($_POST['surname'])){
+		$pername = $_POST['pername'];
+		$surname = $_POST['surname'];
+		$year = $_POST['old'];
+
+		$_SESSION['user_pername'] = $name;
+		$_SESSION['user_surname'] = $password;
+		$_SESSION['user_old'] = $year;
 	}
 ?>
 <!DOCTYPE html>
@@ -26,10 +24,10 @@
 	<div id="content">
 		<form action="" method="post">
 			<h3>Профиль</h3>
-			<input type="text" name="pname" placeholder="Имя"><br>	
-			<input type="text" name="psurname" placeholder="Фамилия"><br>
+			<input type="text" name="pername" value='<?php if(isset($_SESSION['user_pername'])) echo $_SESSION['user_pername'] ?>' placeholder="Имя"><br>	
+			<input type="text" name="surname" placeholder="Фамилия"><br>
 			<input type="text" name="old" placeholder="Возраст"><br>
-			<input type="submit" name="" >Сохранить
+			<input type="submit" value="Сохранить" name="" >
 		</form>
 	</div>
 </body>
